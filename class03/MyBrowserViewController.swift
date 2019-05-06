285ca70523867a82e353efb3c08ba27fb9961a79
Jennifer Lin <chammy1111@gmail.com>
2019-05-06 12:23:52 +0800

    class assignment on May,6

--- class03/MyBrowserViewController.swift
+++ class03/MyBrowserViewController.swift
@@ -31,6 +31,15 @@ class MyBrowserViewController: UIViewController {
     }
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
+        
+        NotificationCenter.default.removeObserver(self,name:
+            UIResponder.keyboardWillShowNotification, object: nil)
+        
+        
+        NotificationCenter.default.removeObserver(self,name:
+            UIResponder.keyboardWillHideNotification, object:nil)
+        
+        
     }
     
     @objc func keyboardWillDisAppear(notification: NSNotification?){
