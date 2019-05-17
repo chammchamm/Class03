fe20c2f184aaa6c6044278372212e55c4c6037e6
Jennifer Lin <chammy1111@gmail.com>
2019-05-17 18:45:25 +0800

    Keyboard appear

--- class03/MyBrowserViewController.swift
+++ class03/MyBrowserViewController.swift
@@ -7,47 +7,64 @@
 //
 
 import UIKit
+import WebKit
 
 class MyBrowserViewController: UIViewController {
 
+    @IBOutlet weak var btnGoBottomConstraint: NSLayoutConstraint!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+//        myWebView.load(URLRequest(url, URL(string: "http://www.google.com")!))
+//        myWebView,load(URLRequest(url,URL(s))
     }
     
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        NotificationCenter.default.addObserver(self,selector:
-            #selector(keyboardWillAppear(notification:)),name:
-            UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillAppear(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
         
         
-        NotificationCenter.default.addObserver(self,selector:
-            #selector(keyboardWillDisAppear(notification:)),name:
-            UIResponder.keyboardWillHideNotification, object:nil)
+//        NotificationCenter.default.addObserver(self,selector:
+//            #selector(keyboardWillDisAppear(notification:)),name:
+//            UIResponder.keyboardWillHideNotification, object:nil)
     }
+    
+    
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         
         NotificationCenter.default.removeObserver(self,name:
             UIResponder.keyboardWillShowNotification, object: nil)
+//
+//
+//        NotificationCenter.default.removeObserver(self,name:
+//            UIResponder.keyboardWillHideNotification, object:nil)
+    }
+    
+    
+    
+
+    @objc func keyboardWillAppear(notification: NSNotification?){
+        print("keyboardWillAppear")
         
+        guard let frame = notification?.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue
+            else {
+                return}
         
-        NotificationCenter.default.removeObserver(self,name:
-            UIResponder.keyboardWillHideNotification, object:nil)
-        
+        self.btnGoBottomConstraint.constant = frame.cgRectValue.height;
         
     }
     
     @objc func keyboardWillDisAppear(notification: NSNotification?){
         print("keyboardWillDisAppear")
+        
+        
     }
     
-    @objc func keyboardWillAppear(notification: NSNotification?){
-        print("keyboardWillAppear")
-    }
+    
     
 }
